a48816d944d94ec6db81797c3f4469de80c251e8
SiceloVilakazi <svilakazi@jhb.dvt.co.za>
2023-05-30 11:41:55 +0200

    style: :art: added media queries
    media queries in place and partially working
    
    currently cannot make use of sass

--- JS/getFlights.js
+++ JS/getFlights.js
@@ -1,7 +1,7 @@
 //needs to be sorted
 import { getAllFlights } from "../API/ApiService.js";
 import {flightHandler} from "../JS/leafletmap.js";
-var listContainer = document.getElementById('flight-table');
+var flightList = document.getElementById('flight-table');
 
 const flights = await getAllFlights();
 console.log(flights);
@@ -13,17 +13,17 @@ for (let i = 0; i < 20; i++) {
   };
   //Fix the dom manipulation
   const flightTable = document.createElement("tr");
-  const flightTableData = document.createElement("td");
-  flightTableData.innerText = flights[i][1];
-  flightTableData.style.color="blue";
-  const flightTableData2 = document.createElement("td");
-  flightTableData2.innerText = flights[i][2];
+  const flightNumber = document.createElement("td");
+  flightNumber.innerText = flights[i][1];
+  flightNumber.style.color="blue";
+  const flightCountry = document.createElement("td");
+  flightCountry.innerText = flights[i][2];
  
 
-  flightTable.appendChild(flightTableData);
-  flightTable.appendChild(flightTableData2);
+  flightTable.appendChild(flightNumber);
+  flightTable.appendChild(flightCountry);
 
-  listContainer.appendChild(flightTable);
+  flightList.appendChild(flightTable);
   coordinates.lat = flights[i][5];
   coordinates.long = flights[i][6];
   flightTable.addEventListener("click", () => flightHandler(coordinates));
