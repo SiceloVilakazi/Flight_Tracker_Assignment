552cadccd6af3e76b704e4fa7661c456b96dfeca
SiceloVilakazi <svilakazi@jhb.dvt.co.za>
2023-06-07 13:55:51 +0200

    feat: :sparkles: added an observable, and local storage, pending polling

--- src/API/ApiService.ts
+++ src/API/ApiService.ts
@@ -1,20 +1,39 @@
 import {FlightDetails} from '../Interfaces/FlightDetails';
+import { Observable, from, timer } from 'rxjs';
+import {switchMap,distinctUntilChanged } from 'rxjs/operators';
 
-export async function GetFlights() {
+export  function GetFlights() :Observable<FlightDetails[]>
+{
  
     let endpoint : string = "https://opensky-network.org/api/states/all";
     let flights : FlightDetails[];
 
-    return  await fetch(endpoint).then((res)=> res.json())
-                          .then((responseJSON) =>{
-                            flights = responseJSON.states.map((state:any) => {
-                                return {
-                                     flightNumber: state[1],
-                                     flightCountry: state[2],
-                                     latitude: state[5],
-                                     longitude: state[6]
-                                     };
-                            }).slice(0,5);
-                            return flights;
-                          });
+        return from(
+            fetch(endpoint).then((res)=> res.json())
+            .then((responseJSON) =>{
+                try{
+                    flights = responseJSON.states.map((state:any) => {
+                        return {
+                             flightNumber: state[1],
+                             flightCountry: state[2],
+                             latitude: state[5],
+                             longitude: state[6]
+                             };
+                    }).slice(0,5);
+                    localStorage.setItem('flights',JSON.stringify(flights));
+                    return flights;
+                }
+                catch{
+                    flights = JSON.parse(localStorage.getItem('flights'));
+                    return flights;
+                } 
+            }
+        ) 
+    );
+}
+
+export function polling(interval:5000): Observable<FlightDetails[]>{
+    return timer(0,interval).pipe(
+        switchMap(_=>GetFlights())
+    );
 }
