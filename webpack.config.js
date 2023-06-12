203c08bfe2850d189fc9cdd2536f037a84a314bb
SiceloVilakazi <svilakazi@jhb.dvt.co.za>
2023-06-12 21:31:30 +0200

    postcss

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,11 @@ module.exports = {
             ],
         },
         {
+          test: /\.css$/,
+          include: path.resolve(__dirname, "src"),
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+        {
             test:/\.(png|svg|jpg|gif)$/i,
             type:'asset/resource'
         },
